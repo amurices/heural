0393e1f77dc50f5bba2f91820f78cf36b7740259
amurices <decomuricy@gmail.com>
2020-07-02 20:34:31 -0300

    Add little main fn with Yuroso (@ycps)

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,21 @@ module Lib
     ) where
 
 import IO.Network
+import qualified Logic.Network as LN
 import System.Random
+import System.Environment
 
 someFunc :: IO ()
-someFunc = makeBrain randomIO (Right randomIO) [3,3,3] >>= print
+someFunc = do 
+    putStrLn "Input the size of each layer of the brain"
+    sizes <- map read . words <$> getLine
+    putStrLn "Input the iteration number"
+    its <- read <$> getLine
+    brain <- makeBrain randomIO (Right randomIO) sizes
+    let input      = replicate (head sizes) 1
+        desiredOut = replicate (last sizes) 6.9420
+    evolvedBrain <- go input desiredOut its brain
+    print (last $ LN.feedBrain input evolvedBrain)
+    where go _ _ 0 evolvedBrain = pure evolvedBrain
+          go input desiredOut its currBrain = go input desiredOut (its - 1) (LN.learn currBrain input desiredOut)
+
